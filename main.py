5b31b70d33ef5d9c2d4e38cf1c42b489c26246e3
Mateus Vieira <mateus17v@gmail.com>
2020-12-03 23:53:21 -0300

    Update - removing print

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from os import path, getcwd, system, name 
 from time import perf_counter, sleep
+from functools import reduce
 
 def read_file(filename):
   try:
@@ -20,27 +21,30 @@ def read_file(filename):
 def rotate(l, n):
   return l[n:] + l[:n]
 
-def shift_data(data):
+def shift_data(data, right_number):
   for line, value in enumerate(data):
-    data[line] = rotate(value, 3)
+    data[line] = rotate(value, right_number)
 
   return data
 
-def count_number_socks(data):
+def count_number_socks(data, right_number, down_number):
   count_of_socks = 0
   flag = True
   length_data = len(data) - 1
+  line = 0
 
-  for line, value in enumerate(data):
-    if length_data == line:
+  while True:
+    if (length_data == line) or (length_data < (line+down_number)) :
       break
 
-    field = data[line+1][3]
+    field = data[line+down_number][right_number]
 
     if field == '#':
       count_of_socks += 1
 
-    data = shift_data(data)
+    data = shift_data(data, right_number)
+
+    line += down_number
 
   return count_of_socks
 
@@ -50,7 +54,25 @@ if __name__ == "__main__":
     filename = path.join(getcwd(), 'inputData.txt')
     input_data = read_file(filename)
 
-    result = count_number_socks(data=input_data)
+    rules = [
+      {'Right': 1, 'Down': 1},
+      {'Right': 3, 'Down': 1},
+      {'Right': 5, 'Down': 1},
+      {'Right': 7, 'Down': 1},
+      {'Right': 1, 'Down': 2},
+    ]
+
+    for item in rules:
+      data = input_data.copy()
+      results.append(
+        count_number_socks(
+            data=data, 
+            right_number=item.get('Right'),
+            down_number=item.get('Down')
+          )
+      )
+
+    result = reduce((lambda x, y: x * y), results)
 
     print(f'Result: {result}')
 
